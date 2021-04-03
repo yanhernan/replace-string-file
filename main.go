3ff2b98de9692475666714965550eb6780fa113b
Yanny Hernandez <yan.hernan.551@gmail.com>
2021-04-03 18:09:27 -0300

    fix in variables

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ import (
 
 func main() {
 	args := os.Args
-	filename := args[0]
+	filename := args[1]
+
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		log.Fatalln("file not exist, " + filename)
 	}
-
-	search := args[1]
-	replace := args[1]
+	log.Println(args)
+	search := args[2]
+	replace := args[3]
 	data, e := ioutil.ReadFile(filename)
 	if e != nil {
 		log.Fatalln(e)
